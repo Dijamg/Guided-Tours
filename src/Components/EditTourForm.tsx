908c8bee03654745f73eed50ebc31d25d250c25a
DijamGoran <diamgoran@gmail.com>
2019-12-28 21:20:34 +0200

    bugfix

--- src/Components/EditTourForm.tsx
+++ src/Components/EditTourForm.tsx
@@ -12,7 +12,6 @@ const EditTourForm = ({ tour, show, handleClose, operators}: {tour: Tour, show: 
     const [selectedPois, setSelectedPois] = useState<Poi[]>(tour.POIs)
 
     const close = () => {
-        setSelectedPois(tour.POIs)
         handleClose()
     }
 
@@ -26,9 +25,9 @@ const EditTourForm = ({ tour, show, handleClose, operators}: {tour: Tour, show: 
                 .update(tour.id, changedTour)
                 .then(returnedTour => {
                     operators.setTours(operators.tours.map(t => t.id !== tour.id ? t : returnedTour))
-                    console.log("Updated Tour " + tour.name)
-                    close()    
-                }) 
+                })
+                console.log("Updated Tour " + tour.name)
+                close()     
         }catch(exception){
             console.log(exception)
         }    
